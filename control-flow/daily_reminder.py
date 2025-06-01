73346d4f62efb007e830421f1aa53f4910eed574
soumaya boumsiss <soumayaboumsiss@gmail.com>
2025-06-01 23:15:26 +0100

    ...

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -10,9 +10,10 @@ match priority:
     case "low":
         pr = "low"
     case _:
-        pr = "unknown"
+        pr = priority  # just keep input if unknown
 
 if time_bound == "yes":
-    print(f"\nReminder: '{task}' is a {pr} priority task that requires immediate attention today!")
-else:
-    print(f"\nNote: '{task}' is a {pr} priority task. Consider completing it when you have free time.")
+    print(f"Reminder: '{task}' is a {pr} priority task that requires immediate attention today!")
+elif time_bound == "no":
+    print(f"Note: '{task}' is a {pr} priority task. Consider completing it when you have free time.")
+
